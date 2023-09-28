f19f48e14ea93dab0dd17d7c7a1ad1379110d887
VukMar <vuk.s.maric@gmail.com>
2023-09-28 21:57:44 +0200

    Initail commit

---
+++ get_track.php
@@ -0,0 +1,101 @@
+<?php
+// Read the access token from the JSON file
+$token_file = 'token.json';
+
+$origin = $_SERVER['HTTP_ORIGIN'];
+if ($origin === 'http://localhost:3000' || $origin === 'http://192.168.1.6:3000') {
+    header("Access-Control-Allow-Origin: " . $origin);
+} else if ($origin === 'https://www.vukmaric.rs' || $origin === 'https://vukmaric.rs') {
+    header("Access-Control-Allow-Origin: " . $origin);
+}
+
+if (file_exists($token_file)) {
+    $token_data = json_decode(file_get_contents($token_file), true);
+
+    if (isset($token_data['access_token'])) {
+        $access_token = $token_data['access_token'];
+
+        // Use the access token to fetch user information from Deezer
+        $api_url = "https://api.deezer.com/user/me/history?access_token=" . $access_token;
+
+        // Initialize a cURL session
+        $ch = curl_init($api_url);
+
+        // Set cURL options
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // Execute the cURL request to get user information
+        $user_info_response = curl_exec($ch);
+
+        // Check for cURL errors
+        if (curl_errno($ch)) {
+            die('cURL error: ' . curl_error($ch));
+        }
+
+        // Close cURL session
+        curl_close($ch);
+
+        // Parse the JSON response for user information
+        $user_history = json_decode($user_info_response, true);
+
+        $jsonFileName = 'Track.json';
+        $newTrackData = [
+            "data" => $user_history['data'][0],
+            "date" => date('d-m-Y H:i') // Use 24-hour format (H:i)
+        ];
+
+        // Check if the file exists
+        if (file_exists($jsonFileName)) {
+            // Read the JSON file
+            $jsonData = file_get_contents($jsonFileName);
+
+            // Parse the JSON data into a PHP associative array
+            $storedTrackData = json_decode($jsonData, true);
+
+            if ($storedTrackData !== null) {
+                if ($storedTrackData['data']['id'] === $newTrackData['data']['id']) {
+                    $date1 = $storedTrackData['date'];
+                    $date2 = $newTrackData['date'];
+
+                    // Calculate the time difference with the current date and time
+                    $dateTime1 = DateTime::createFromFormat('d-m-Y H:i', $date1);
+                    $dateTime2 = DateTime::createFromFormat('d-m-Y H:i', $date2);
+                    $interval = $dateTime1->diff($dateTime2);
+                    $totalMinutesPassed = $interval->days * 24 * 60 + $interval->h * 60 + $interval->i;
+                    
+                    if ($totalMinutesPassed > 10) {
+                        $data = [
+                            "data" => null,
+                            "date" => date('d-m-Y H:i') // Use 24-hour format (H:i)
+                        ];
+                        //Send the blank data
+                        echo json_encode($data);
+                    } else {
+                        //if 10 minutes have not passed send th stored track data
+                        echo json_encode($storedTrackData);
+                    }
+                } else {
+                    //if no id match is found make new record to Track.json
+                    file_put_contents($jsonFileName, json_encode($newTrackData));
+                    //send new track data
+                    echo json_encode($newTrackData);
+                }
+            } else {
+                //if no data in Track.json is found make new record
+                file_put_contents($jsonFileName, json_encode($newTrackData));
+                //Send the obtained data
+                echo json_encode($newTrackData);
+            }
+        } else {
+            //If no Track.json is found create Track.json and append track data
+            file_put_contents($jsonFileName, json_encode($newTrackData));
+            //Send the new data
+            echo json_encode($newTrackData);
+        }
+    } else {
+        echo "Access token not found in the JSON file.";
+    }
+} else {
+    echo "Token file not found.";
+}
+?>
---
+++ listening.php
@@ -0,0 +1,59 @@
+<?php
+$app_id = "635261";
+$app_secret = "1fded776787efcedf1aea1cf7daade91";
+$my_url = "https://backend.vukmaric.rs/api/deezer/listening.php";
+
+session_start();
+$code = $_REQUEST["code"];
+
+if (empty($code)) {
+    $_SESSION['state'] = md5(uniqid(rand(), TRUE)); // CSRF protection
+
+    $dialog_url = "https://connect.deezer.com/oauth/auth.php?app_id=" . $app_id
+        . "&redirect_uri=" . urlencode($my_url) . "&perms=email,offline_access,listening_history"
+        . "&state=" . $_SESSION['state'];
+
+    header("Location: " . $dialog_url);
+    exit;
+} else {
+    if ($_REQUEST['state'] == $_SESSION['state']) {
+        $token_url = "https://connect.deezer.com/oauth/access_token.php?app_id="
+            . $app_id . "&secret="
+            . $app_secret . "&code=" . $code;
+
+        // Initialize cURL session
+        $ch = curl_init($token_url);
+
+        // Set cURL options
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // Execute the cURL request to get the token response
+        $response = curl_exec($ch);
+
+        // Check for cURL errors
+        if (curl_errno($ch)) {
+            die('cURL error: ' . curl_error($ch));
+        }
+
+        // Close cURL session
+        curl_close($ch);
+
+        parse_str($response, $token_data);
+
+        // Check if the access token was obtained successfully
+        if (isset($token_data['access_token'])) {
+            $access_token = $token_data['access_token'];
+
+            // Store the access token in a JSON file
+            $token_data = ['access_token' => $access_token];
+            file_put_contents('token.json', json_encode($token_data));
+
+            echo "Access token obtained successfully and stored.";
+        } else {
+            echo "Access token not found in the response.";
+        }
+    } else {
+        echo "The state does not match. You may be a victim of CSRF.";
+    }
+}
+?>
